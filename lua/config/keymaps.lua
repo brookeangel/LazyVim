5ff216d599c89e2dad10ebc88ee4dd58bf93a71f
Brooke <brooke.angel@ridewithvia.com>
2026-03-30 15:03:31 -0700

    Config, mostly from lazy starter

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+-- Move to window using the <ctrl> hjkl keys
+-- TODO: figure me out one day
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
