1e92a9a88a5a47895da4423f8bf5fedf87efb594
Brooke <brooke.angel@ridewithvia.com>
2026-03-30 14:38:46 -0700

    Add some custom plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        ruby_lsp = { enabled = false },
+        ts_ls = { enabled = false },
+        solargraph = {
+          enabled = true,
+          root_dir = require("lspconfig.util").root_pattern(".git"),
+          cmd = { "bundle", "exec", "solargraph", "stdio" },
+        },
+      },
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    opts = {
+      settings = { expose_as_code_action = { "add_missing_imports" } },
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+  },
+}
