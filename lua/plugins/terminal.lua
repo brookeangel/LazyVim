fad58fc32cddf3bf7c2020a0ab7e0588da74504a
Brooke Angel <brooke.angel@ridewithvia.com>
2026-07-07 10:44:08 -0700

    Pin snacks terminal to bottom

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    terminal = {
+      win = {
+        position = "bottom", -- Options are "bottom", "top", "left", "right", or "float"
+        height = 0.4, -- Sets height to 40% of the screen
+      },
+    },
+  },
+}
